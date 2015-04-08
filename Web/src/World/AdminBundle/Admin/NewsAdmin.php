005e54121f9476bba75ff262abffc35ec202528e
Jose Alfredo Alvarez Aldana <josealfredo1515@gmail.com>
2015-04-08 16:29:04 -0700

    AdminBundle integration with Sonata.

---
+++ Web/src/World/AdminBundle/Admin/NewsAdmin.php
@@ -0,0 +1,72 @@
+<?php
+// src/Acme/DemoBundle/Admin/PostAdmin.php
+
+namespace World\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class NewsAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+//            ->add('title', 'text', array('label' => 'Post Title'))
+//            ->add('author', 'entity', array('class' => 'Acme\DemoBundle\Entity\User'))
+            ->add('name') //if no type is specified, SonataAdminBundle tries to guess it
+            ->add('description')
+            ->add('image', 'file', array('required' => false, 'data_class' => null, 'mapped' => true))
+            ->add('startDate')
+            ->add('endDate')
+            ->add('sortOrder')
+            ->add('enabled')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('description')
+            ->add('image')
+            ->add('startDate')
+            ->add('endDate')
+            ->add('sortOrder')
+            ->add('enabled')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('description')
+            ->add('image', null, array(
+                'template' => 'WorldToolBundle:Admin:image.html.twig'
+            ))
+            ->add('startDate')
+            ->add('endDate')
+            ->add('sortOrder')
+            ->add('enabled')
+        ;
+    }
+
+    public function prePersist($object)
+    {
+        // We get the uploadable manager!
+        $uploadableManager = $this->getConfigurationPool()->getContainer()->get('stof_doctrine_extensions.uploadable.manager');
+        $uploadableManager->markEntityToUpload($object, $object->getImage());
+    }
+
+    public function preUpdate($object)
+    {
+        // We get the uploadable manager!
+        $uploadableManager = $this->getConfigurationPool()->getContainer()->get('stof_doctrine_extensions.uploadable.manager');
+        $uploadableManager->markEntityToUpload($object, $object->getImage());
+    }
+}
